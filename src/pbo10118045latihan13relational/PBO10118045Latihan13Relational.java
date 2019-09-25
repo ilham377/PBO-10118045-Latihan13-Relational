65cd2900ccffcf95da4eaacc5df37840c8c8e9dd
Ilham <Ilham@LAPTOP-G4VGSH61>
2019-09-25 21:14:11 +0700

    first commit

---
+++ src/pbo10118045latihan13relational/PBO10118045Latihan13Relational.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10118045latihan13relational;
+
+/**
+ *
+ * @author 
+ * Nama     :   Muhammad Ilham Apriyadi
+ * Kelas    :   IF 2
+ * NIM      :   10118045
+ * Deskripsi Program : Relational
+ */
+public class PBO10118045Latihan13Relational {
+
+   
+    public static void main(String[] args) {
+        int bi11 = 10;
+        int bi12 = 20;
+        System.out.println("a == b = " + (bi11 == bi12));
+        System.out.println("a != b = " + (bi11 != bi12));
+        System.out.println("a > b = " + (bi11 > bi12));
+        System.out.println("a < b = " + (bi11 < bi12));
+        System.out.println("a >= b = " + (bi12 >= bi11));
+        System.out.println("a <= b = " + (bi12 <= bi11));
+    }
+    
+}
